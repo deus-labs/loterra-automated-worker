57ddccf60e4653bbc886e5ac8ac31919c2fc8606
0xantman <antho74830@gmail.com>
2021-03-01 12:15:10 +0100

    Fist commit

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const {
+    LCDClient,
+    MsgExecuteContract,
+    MnemonicKey
+} = require('@terra-money/terra.js')
+
+const mk = new MnemonicKey({
+    mnemonic:
+    process.env.MNEMONIC,
+});
+const terra = new LCDClient({
+    URL: process.env.LCD_URL,
+    chainID: process.env.CHAIN_ID,
+})
+const wallet = terra.wallet(mk)
+
+function try_play(){
+
+    setInterval(function(){
+       const send = new MsgExecuteContract(
+            mk.accAddress,
+            process.env.LOTERRA_CONTRACT,
+            {
+                play: {},
+            }
+        )
+        wallet
+            .createAndSignTx({
+                msgs: [send],
+                memo: 'play from automated worker!',
+            })
+            .then(tx => terra.tx.broadcast(tx))
+            .then(result => {
+                console.log(`TX hash: ${result.txhash}`);
+            }).catch(err => console.log(err));
+    }, 60000);
+}
+
+try_play()
