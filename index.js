2f595de688204fa6d527aaa0e6f92ac6a50b3379
0xantman <antho74830@gmail.com>
2021-06-05 13:52:16 +0200

    fix

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ const fees = new StdFee(1_000_000, { uusd: 200000 })
 function worker() {
 
     setInterval(async function(){
-        let _round
-        let signature_base64
-        let previous_signature_base64
         try {
             let res = await terra.wasm.contractQuery(
                 process.env.LOTERRA_CONTRACT,
@@ -32,35 +29,36 @@ function worker() {
             );
             const res2 = await axios.get(`https://drand.cloudflare.com/public/${res.next_round}`)
             const {round, signature, previous_signature} = res2.data
-            _round = round
-            signature_base64 = Buffer.from(signature, 'hex').toString('base64')
-            previous_signature_base64 = Buffer.from(previous_signature, 'hex').toString('base64')
+            let _round = round
+            let signature_base64 = Buffer.from(signature, 'hex').toString('base64')
+            let previous_signature_base64 = Buffer.from(previous_signature, 'hex').toString('base64')
 
+            const msg = new MsgExecuteContract(mk.accAddress, process.env.TERRAND_CONTRACT, {
+                drand:{
+                    round: _round,
+                    previous_signature: previous_signature_base64,
+                    signature: signature_base64
+                }
+            })
+
+            wallet
+                .createAndSignTx({
+                    msgs: [msg],
+                    memo: 'Automated worker add randomness!',
+                    fee: new StdFee(7_000_000, { uusd: 2000000 })
+                })
+                .then(tx => terra.tx.broadcast(tx))
+                .then(result => {
+                    console.log(`TX hash: ${result.txhash}`);
+                }).catch(e => console.log(e));
 
         } catch (e) {
             console.log(e)
         }
-        const msg = new MsgExecuteContract(mk.accAddress, process.env.TERRAND_CONTRACT, {
-            drand:{
-                round: _round,
-                previous_signature: previous_signature_base64,
-                signature: signature_base64
-            }
-        })
+
         const msg1 = new MsgExecuteContract(mk.accAddress, process.env.LOTERRA_CONTRACT, {
             play: {}
         })
-
-        wallet
-            .createAndSignTx({
-                msgs: [msg],
-                memo: 'Automated worker add randomness!',
-                fee: new StdFee(7_000_000, { uusd: 2000000 })
-            })
-            .then(tx => terra.tx.broadcast(tx))
-            .then(result => {
-                console.log(`TX hash: ${result.txhash}`);
-            }).catch(e => console.log(e));
 
         wallet
             .createAndSignTx({
